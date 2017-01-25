f246511bb81de154e6311d4e4812db08aba7558b
JasonRoyalHart <jason@jasonrhart.com>
2017-01-25 08:06:24 -0600

    First commit

---
+++ gui.js
@@ -0,0 +1,33 @@
+var people = [];
+function initSearch(people){
+    var x = document.getElementById("nameSearch").value;
+    alert ("You typed: " + x);
+
+}
+function initSearchByName(){
+	;
+}
+function initSearchByTraits(){
+	;
+}
+function displayResults(){
+	;
+}
+function isNumeric() {
+	;
+}
+function getAge(){
+	;
+}
+function getHeight(){
+	;
+}
+function getWeight(){
+	;
+}
+function getEye(){
+	;
+}
+function getOccupation(){
+	;
+}
